03338a1835ee739c9a97e6a171bde555e66c7e38
chendeng <chendeng@2014outlook.com>
2018-01-26 17:37:34 +0800

    播放器缩放动画实现，用github插件

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="player" v-show="playlist.length>0">
-    <transition name="normal">
-      <div class="normal-player"  v-show="fullScreen">
+    <transition name="normal"
+                @enter="enter"
+                @after-enter="afterEnter"
+                @leave="leave"
+                @after-leave="afterLeave"
+    >
+      <div class="normal-player" v-show="fullScreen">
         <div class="background">
           <img :src="currentSong.image" width="100%" height="100%" alt="">
         </div>
@@ -14,7 +19,7 @@
         </div>
         <div class="middle">
           <div class="middle-l">
-            <div class="cd-wrapper">
+            <div class="cd-wrapper" ref="cd-wrapper">
               <div class="cd">
                 <img class="image" :src="currentSong.image" alt="">
               </div>
@@ -64,10 +69,15 @@
 
 <script type="text/ecmascript-6">
   import {mapGetters, mapMutations} from 'vuex'
+  import animations from 'create-keyframe-animation'
+  import {prefixStyle} from 'common/js/dom'
   export default{
+    created(){
+      this._getPosAndScale()
+    },
     methods: {
       back(){
-          this.setFullScreen(false)
+        this.setFullScreen(false)
 
       },
       open(){
@@ -75,7 +85,64 @@
       },
       ...mapMutations({
         setFullScreen: 'SET_FULL_SCREEN'
-      })
+      }),
+      enter(el, done) {
+        const {x, y, scale} = this._getPosAndScale()
+
+        let animation = {
+          0: {
+            transform: `translate3d(${x}px,${y}px,0) scale(${scale})`
+          },
+          60: {
+            transform: `translate3d(0,0,0) scale(1.1)`
+          },
+          100: {
+            transform: `translate3d(0,0,0) scale(1)`
+          }
+        }
+
+        animations.registerAnimation({
+          name: 'move',
+          animation,
+          presets: {
+            duration: 400,
+            easing: 'linear'
+          }
+        })
+
+        animations.runAnimation(this.$refs.cdWrapper, 'move', done)
+      },
+      afterEnter(){
+        animations.unregisterAnimation('move')
+        this.$refs.cdWrapper.style.animation = ''
+      },
+      leave(el, done) {
+        this.$refs.cdWrapper.style.animation = 'all .4s'
+        const {x, y, scale} = this._getPosAndScale()
+        this.$refs.cdWrapper.style[transform] = `translate3d(${x}px,${y}px,0) scale(${scale})`
+        this.$refs.cdWrapper.addEventListener('transitionend', done)//监听完成动画之后的事件
+
+      },
+      afterLeave() {
+        this.$refs.cdWrapper.style.transition = ''
+        this.$refs.cdWrapper.style[transform] = ''
+
+      },
+      _getPosAndScale(){
+//        计算出动画过度的位置，从初始位置到过度的位置
+        const targetWidth = 40
+        const paddingLeft = 40
+        const paddingBottom = 30
+        const paddingTop = 80
+        const width = window.innerWidth * 0.8
+        const scale = targetWidth / width
+        const x = -(window.innerWidth / 2 - paddingLeft)
+        const y = window.innerHeight - paddingTop - width / 2 - paddingBottom
+
+        return {
+          x, y, scale
+        }
+      }
 
     },
     computed: {
